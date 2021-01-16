42254650c0667c48725e0248f6bd12c899214ac8
James Chen <zc1069@nyu.edu>
2021-01-16 18:33:03 -0500

    init

---
+++ app/code/SD/Test/Controller/Router.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace SD\Test\Controller;
+
+class Router implements \Magento\Framework\App\RouterInterface
+{
+    /**
+     * @var \Magento\Framework\App\ActionFactory
+     */
+    protected $actionFactory;
+
+    /**
+     * Event manager
+     *
+     * @var \Magento\Framework\Event\ManagerInterface
+     */
+    protected $_eventManager;
+
+    /**
+     * Store manager
+     *
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * Page factory
+     *
+     * @var \Magento\Cms\Model\PageFactory
+     */
+    protected $_pageFactory;
+
+    /**
+     * Config primary
+     *
+     * @var \Magento\Framework\App\State
+     */
+    protected $_appState;
+
+    /**
+     * Url
+     *
+     * @var \Magento\Framework\UrlInterface
+     */
+    protected $_url;
+
+    /**
+     * Response
+     *
+     * @var \Magento\Framework\App\ResponseInterface
+     */
+    protected $_response;
+
+    /**
+     * @param \Magento\Framework\App\ActionFactory $actionFactory
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
+     * @param \Magento\Framework\UrlInterface $url
+     * @param \Magento\Cms\Model\PageFactory $pageFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\App\ResponseInterface $response
+     */
+    public function __construct(
+        \Magento\Framework\App\ActionFactory $actionFactory,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Framework\UrlInterface $url,
+        \Magento\Cms\Model\PageFactory $pageFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\App\ResponseInterface $response
+    ) {
+        $this->actionFactory = $actionFactory;
+        $this->_eventManager = $eventManager;
+        $this->_url = $url;
+        $this->_pageFactory = $pageFactory;
+        $this->_storeManager = $storeManager;
+        $this->_response = $response;
+    }
+
+    public function match(\Magento\Framework\App\RequestInterface $request)
+    {
+        // $identifier = trim($request->getPathInfo(), '/');
+
+        // $condition = new \Magento\Framework\DataObject(['identifier' => $identifier, 'continue' => true]);
+
+        // $identifier = $condition->getIdentifier();
+
+        $request->setModuleName('sd')->setControllerName('test')->setActionName('view')->setParam('page_id', "sds");
+        // $request->setAlias(\Magento\Framework\Url::REWRITE_REQUEST_PATH_ALIAS, $identifier);
+
+        return $this->actionFactory->create(\Magento\Framework\App\Action\Forward::class);
+    }
+}
---
+++ app/code/SD/Test/Controller/Test/View.php
@@ -0,0 +1,21 @@
+<?php
+namespace SD\Test\Controller\Test;
+
+class View extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+        die("sdfsdf");
+		return $this->_pageFactory->create();
+	}
+}
