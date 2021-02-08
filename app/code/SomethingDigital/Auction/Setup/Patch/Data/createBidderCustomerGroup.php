fec63f90fda37dffeaa8564c10c23539c62436c0
James Chen <zc1069@nyu.edu>
2021-02-08 12:43:35 -0500

    Added data patch to create Bidder customer group

---
+++ app/code/SomethingDigital/Auction/Setup/Patch/Data/createBidderCustomerGroup.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace SomethingDigital\Auction\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Customer\Model\GroupFactory;
+
+/**
+* Patch is mechanism, that allows to do atomic upgrade data changes
+*/
+class createBidderCustomerGroup implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface $moduleDataSetup
+     */
+    private $moduleDataSetup;
+    /**
+     * @var GroupFactory
+     */
+    private $groupFactory;
+
+    /**
+     * @param GroupFactory $groupFactory
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     */
+    public function __construct(
+        GroupFactory $groupFactory,
+        ModuleDataSetupInterface $moduleDataSetup
+    ) {
+        $this->groupFactory = $groupFactory;
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * Do Upgrade
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        // Create the new group
+        /** @var \Magento\Customer\Model\Group $group */
+        $group = $this->groupFactory->create();
+        $group
+            ->setCode('Bidder')
+            ->setTaxClassId(3)
+            ->save();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+
+        ];
+    }
+}
